f45125938ac31666ab88d52348951183723274ee
dorodnyy.dmitry <dorodnyy.dmitry@gmail.com>
2016-05-09 22:27:22 +0300

    fxd

--- VAR2_3.cpp
+++ VAR2_3.cpp
@@ -10,11 +10,10 @@ int main()
     string shortestWord; //запомнить самое короткое
     string preLastWord; //запомнить предпоследнее
 
-
     int wordPosition = 0; // счетчик, чтобы знат ькогда вставлять то или иное слдово
-    int shortestWordLength; //длина самого короткого
-    int wordsAmount; //число слов
-    int shortestWordPosition; //позиция самого короткого слова в строке
+    int shortestWordLength = 0; //длина самого короткого
+    int wordsAmount = 0; //число слов
+    int shortestWordPosition = 0; //позиция самого короткого слова в строке
 
     getline(cin, data);
 
@@ -36,11 +35,11 @@ int main()
         if(word.size() < shortestWordLength)
         {
             shortestWord = word;
+            shortestWordLength = word.size();
             shortestWordPosition = wordPosition; // запомним, где самое короткое, чтобы знать ,куда тыкать предпоследнее
         }
         if(wordPosition == wordsAmount - 1)
             preLastWord = word;
-
     }
 
     wordPosition = 0;
@@ -49,14 +48,13 @@ int main()
     {
         wordPosition++;
         if(wordPosition == shortestWordPosition) //тут расставим приоритеты: по логике, вначале встретится замое короткое (разве что оно последнее, на это отдельнавя проверка)
-            cout << preLastWord;                 //значит, вначале ищем куда ткнуть предпоследнее на место короткого
+            cout << preLastWord << " ";                 //значит, вначале ищем куда ткнуть предпоследнее на место короткого
         else                                     //если не находим, смотрим, вдруг это уже предпоследнее слово?
             if(wordPosition == wordsAmount - 1)
-                cout << shortestWord;
+                cout << shortestWord << " ";
             else                                 //если позиция не "критическая", просто бахаем текущее слово, ни на что не заменяя
-                cout << word;
+                cout << word << " ";
     }
-
     return 0;
 }
 
