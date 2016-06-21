a0722fe061456a5c198954a2c022f19314c31ee3
dorodnyyDmitry <dorodnyy.dmitry@gmail.com>
2016-06-21 14:55:13 +0300

    Upper menu scroller

---
+++ menu.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    var header_height = 40; //есть шапка
+    var header_hidden = 0;  //проскроллили ниже
+    var menu = $('#menu'); // ID для меню
+    var top = $(this).scrollTop();
+
+    if(top > header_height)
+        menu.css('top', 40);
+
+    $(window).scroll(function()
+    {
+        top = $(this).scrollTop();
+
+        if(top + header_hidden < header_height)
+            menu.css('top', (header_height - top));
+        else
+            menu.css('top', header_hidden);
+    });
+});
